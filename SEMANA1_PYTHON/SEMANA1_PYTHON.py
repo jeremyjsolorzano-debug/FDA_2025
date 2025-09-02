5898cabb5822dc7a0756f5379b38935befe15d1b
Jeremy <jeremy.jsolorzano@gmail.com>
2025-09-02 15:30:42 -0500

    subiendo ejercicio1-SEM3

---
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -0,0 +1,7 @@
+def ejer1():
+   nombre = input("Ingrese su nombreP")
+   carrera = input("ingrese su carrera")
+
+   print(f"\n{nombre}, bienvenido a FDA de {carrera}")#f es igual que el + o el & espara concatenar por interpolacion
+
+   ejer1()
---
+++ semana1_para_c#/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana1_para_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+           
+        }
+
+       static void ejer1()
+        {
+            string nombre, carrera;//declarando variables
+
+            Console.Write("ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido a FA de {carrera}");
+            // $ significa que conjcatenacion por interpolacion que se usa cuando hay muchas variables a concatenar
+            // \n sirve para el salto de linea   
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+
+    }
+}
